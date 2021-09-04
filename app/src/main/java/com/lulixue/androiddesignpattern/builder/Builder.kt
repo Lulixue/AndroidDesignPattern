66bb1f324953dc59215839d36e8a3a1c8b22ce39
lulixue <jackreagan@163.com>
2021-09-04 21:55:19 +0800

    添加建造者模式

--- app/src/main/java/com/lulixue/androiddesignpattern/builder/Builder.kt
+++ app/src/main/java/com/lulixue/androiddesignpattern/builder/Builder.kt
@@ -2,10 +2,21 @@ package com.lulixue.androiddesignpattern.builder
 
 import android.app.Activity
 import android.app.AlertDialog
-import android.app.Application
 import android.content.Context
+import java.lang.Exception
+import java.lang.RuntimeException
+import java.util.*
 
-class BuilderParent {
+
+fun testBuilder() {
+    val citizen = Citizen.Builder("张三", 23, "374-283432")
+        .setReligion("佛教")
+        .create()
+}
+
+class Citizen private constructor(val name: String, val age: Int, val id: String,
+                                  val birthYear: Int, val religion: String){
+
     // 经典的建造者模式为AlertDialog
     // Builder()负责对象的构建，采用链式调用的方式
     // 对类成员定制，屏蔽了实例化类对象的细节，在create时可以做统一的数据校验
@@ -22,7 +33,28 @@ class BuilderParent {
 
 
     class Builder(val name: String, val age: Int, val id: String) {
-        var birthYear: Int = 0
+        companion object {
+            val thisYear = Calendar.YEAR
+        }
 
+        private var birthYear: Int = 0 // 出生年份可由id算出
+        private var religion: String = "None" // 宗教信仰
+
+        fun setReligion(rel: String): Builder {
+            this.religion = rel
+            return this
+        }
+        // 检测数据是否合法
+        private fun idToYear(id: String): Int {
+            val year = id.substring(0, 4).toInt()
+            if (year > thisYear) {
+                throw RuntimeException("invalid id")
+            }
+            return year
+        }
+
+        fun create(): Citizen {
+            return Citizen(name, age, id, idToYear(id), religion)
+        }
     }
 }
